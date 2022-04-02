0a88fb0e78ef6a1f8f4767ef085c3ec71bc202b7
breakndream <kang26d@mtholyoke.edu>
2022-04-02 15:23:32 -0400

    Sharon's part

---
+++ Pods/Target Support Files/Pods-codeBlanch-codeBlanchUITests/Pods-codeBlanch-codeBlanchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codeBlanch_codeBlanchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_codeBlanch_codeBlanchUITests
+@end
--- codeBlanch/AppDelegate.swift
+++ codeBlanch/AppDelegate.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "X3EMTBiJvhinLIReoyprKKnXkwXuJ2teOtlSpBck"
+                $0.clientKey = "gm5zb19YZsCatySGAbs1wD2RCkzBOPqdfApGZ4Sr"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
         return true
     }
 
@@ -30,7 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
